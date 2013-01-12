4e0c15767311c2d08b6abcfa6b006755452f64e7
Karl Trygve Kalleberg <karltk@boblycat.org>
2013-01-13 00:51:42 +0100

    Add enumerate_ranges for process and module

--- zed/bindings/pyfrida/frida.py
+++ zed/bindings/pyfrida/frida.py
@@ -4,30 +4,53 @@ import threading
 import traceback
 
 
-class Error(Exception):
-    pass
+def attach(pid, device_id = None):
+    return Process(_frida.attach(pid))
 
-def execute_script(script):
+class Process:
+    def __init__(self, session):
+        self._session = session
 
-    def msg(message, data):
-        if message['type'] == 'send':
-            result['data'] = message['payload']
-        elif message['type'] == 'error':
-            result['error'] = message['description']
-        event.set()
+    def enumerate_modules(self):
+        script = self._session.create_script(
+"""
+var modules = [];
+Process.enumerateModules({
+    onMatch: function(name, address, path) {
+        modules.push({name: name, address: address, path: path});
+    },
+    onComplete: function() {
+        send(modules);
+    }
+});
+""")
 
-    result = {}
-    event = threading.Event()
+        result = execute_script(script)
 
-    script.on('message', msg)
-    script.load()
-    event.wait()
-    script.unload()
+        for data in result:
+          yield Module(data['name'], data['address'], data['path'], self._session)
 
-    if 'error' in result:
-        raise Error, result['error']
+    """
+      @param protection example '--x'
+    """
+    def enumerate_ranges(self, protection):
+        script = self._session.create_script(
+"""
+var ranges = [];
+Process.enumerateRanges(\"%s\", {
+    onMatch: function(address, size, protection) {
+        ranges.push({address: address, size: size, protection: protection});
+    },
+    onComplete: function() {
+        send(ranges);
+    }
+});
+""" % protection)
 
-    return result['data']
+        result = execute_script(script)
+
+        for data in result:
+            yield Range(data['address'], data['size'], data['protection'])
 
 class Module:
     def __init__(self, name, address, path, _session):
@@ -54,37 +77,63 @@ Module.enumerateExports(\"%s\", {
         for export in result:
           yield Export(export["name"], export["address"])
 
+    """
+      @param protection example '--x'
+    """
+    def enumerate_ranges(self, protection):
+        script = self._session.create_script(
+"""
+var ranges = [];
+Module.enumerateRanges(\"%s\", \"%s\", {
+    onMatch: function(address, size, protection) {
+        ranges.push({address: address, size: size, protection: protection});
+    },
+    onComplete: function() {
+        send(ranges);
+    }
+});
+""" % (self.name, protection))
+
+        result = execute_script(script)
+
+        for data in result:
+            yield Range(data['address'], data['size'], data['protection'])
+
 class Export:
     def __init__(self, name, address):
         self.name = name
         self.address = address
 
-class Process:
+class Range:
+    def __init__(self, address, size, protection):
+      self.address = address
+      self.size = size
+      self.protection = protection
 
-    def __init__(self, session):
-        self._session = session
+class Error(Exception):
+    pass
 
-    def enumerate_modules(self):
-        script = self._session.create_script(
-"""
-var modules = [];
-Process.enumerateModules({
-    onMatch: function(name, address, path) {
-        modules.push({name: name, address: address, path: path});
-    },
-    onComplete: function() {
-        send(modules);
-    }
-});
-""")
+def execute_script(script):
 
-        result = execute_script(script)
+    def msg(message, data):
+        if message['type'] == 'send':
+            result['data'] = message['payload']
+        elif message['type'] == 'error':
+            result['error'] = message['description']
+        event.set()
 
-        for data in result:
-          yield Module(data['name'], data['address'], data['path'], self._session)
+    result = {}
+    event = threading.Event()
 
+    script.on('message', msg)
+    script.load()
+    event.wait()
+    script.unload()
 
-def attach(pid, device_id = None):
-    return Process(_frida.attach(pid))
+    if 'error' in result:
+        raise Error, result['error']
+
+    return result['data']
+
 
 
